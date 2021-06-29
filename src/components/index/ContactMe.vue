45ad6d0c66a320b17beb3c5f8bfa840cbdf82f10
Luke Joseph <luke@trackwire.io>
2021-06-29 16:29:27 +0200

    remove margin top from the email icon in contact section

--- src/components/index/ContactMe.vue
+++ src/components/index/ContactMe.vue
@@ -83,7 +83,7 @@
 
         <div class="flex items-center mb-3">
 
-          <a href="https://www.linkedin.com/in/luke-joseph-b20413b2/" class="w-5 h-5 mt-1">
+          <a href="https://www.linkedin.com/in/luke-joseph-b20413b2/" class="w-5 h-5">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-mail">
               <path class="fill-primary" d="M22 8.62V18a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V8.62l9.55 4.77a1 1 0 0 0 .9 0L22 8.62z"/>
               <path class="fill-secondary" d="M12 11.38l-10-5V6c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v.38l-10 5z"/>
