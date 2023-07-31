0984fb25dfca910f1f1a16c53211ecc8ebd07a4d
Luke Joseph <luke@trackwire.io>
2023-07-31 19:01:17 +0200

    add builded assets with new website link

--- dist/js/NotFoundComponent.e9724b50.js
+++ dist/js/NotFoundComponent.e9724b50.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["NotFoundComponent"],{2946:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},o=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"text-center about text-grey-300 w-1/2 h-screen"},[n("h1",{staticClass:"text-3xl font-bold tracking-wide mb-12"},[t._v("Whoops 404")]),n("p",{staticClass:"mb-4 text-grey-500 text-xl"},[t._v("page not found!")])])}],a=n("2877"),c={},l=Object(a["a"])(c,s,o,!1,null,null,null);e["default"]=l.exports}}]);
-//# sourceMappingURL=NotFoundComponent.3117ef2e.js.map
+//# sourceMappingURL=NotFoundComponent.e9724b50.js.map
